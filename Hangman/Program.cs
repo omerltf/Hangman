4a7774a2185bbe789d6df76d3feeddb089e2d107
omerltf <omerltf@gmail.com>
2019-08-13 23:23:51 -0400

    Working Game 1.0 (in Beta Testing)

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Hangman {
     class Program {
+        static int incorrectGuesses = 0;
 
         public static List<string> GetPhrases() {
             List<string> phrasesList = new List<string>();
@@ -25,36 +26,22 @@ namespace Hangman {
             return phrases.ElementAt(randomInteger);
         }
 
-        public static void PlayGame(List<string> phrases) {
-            string phrase;
-            phrase = SelectPhrase(phrases);
-            char[] phraseCharacters = GetPhraseCharacters(phrase);
-            HashSet<char> phraseDistinctCharacters = GetPhraseDistinctCharacters(phraseCharacters); 
-        }
-
-        public static char[] GetPhraseCharacters(string phrase) {
-            phrase = phrase.ToUpper();
-            char[] phraseArray = phrase.ToCharArray();
-            return phraseArray;
-        }
-
-        public static void DisplayPhrase (char[] phrase, List<char> phraseGuessedCharacters) {
-            //List<char> phraseGuessedCharacters = new List<char>();
-            for (int count=0; count<phrase.Length; count++) {
-                if (phrase[count]==' ' || phraseGuessedCharacters.Contains(phrase[count])) {
+        public static void DisplayPhrase(char[] phrase, List<char> phraseGuessedCharacters) {
+            for (int count = 0; count < phrase.Length; count++) {
+                if (phrase[count] == ' ' || phraseGuessedCharacters.Contains(phrase[count])) {
                     Console.Write(phrase[count]);
                 }
                 else {
-                    Console.Write("*");
+                    Console.Write("X");
                 }
-               
             }
+            Console.WriteLine();
         }
 
-        public static HashSet<char> GetPhraseDistinctCharacters (char[] phraseCharacters) {
+        public static HashSet<char> GetPhraseDistinctCharacters(char[] phraseCharacters) {
             HashSet<char> distinctChars = new HashSet<char>();
-            for (int count=0; count<phraseCharacters.Length; count++) {
-                if (phraseCharacters[count]==' ') {
+            for (int count = 0; count < phraseCharacters.Length; count++) {
+                if (phraseCharacters[count] == ' ') {
                     continue;
                 }
                 else if (distinctChars.Contains(phraseCharacters[count])) {
@@ -65,6 +52,75 @@ namespace Hangman {
                 }
             }
             return distinctChars;
+        }
+
+        public static char[] GetPhraseCharacters(string phrase) {
+            phrase = phrase.ToUpper();
+            char[] phraseArray = phrase.ToCharArray();
+            return phraseArray;
+        }
+
+        public static char GetCharacterGuess(List<char> guessedCharacters) {
+            while (true) {
+                Console.Write("What do you guess (A-Z)? ");
+                ConsoleKeyInfo selectedOption = Console.ReadKey();
+                Console.WriteLine();
+                char tempChar = selectedOption.KeyChar;
+                tempChar = char.ToUpper(tempChar);
+                if (!(tempChar >= 'A' && tempChar <= 'Z')) {
+                    Console.WriteLine("Incorrect input. Please try again.");
+                    continue;
+                }
+                else if (guessedCharacters.Contains(tempChar)) {
+                    Console.WriteLine("You already guessed {0}. Please try again.", tempChar);
+                    continue;
+                }
+                else {
+                    Console.Clear();
+                    return tempChar;
+                }
+            }
+        }
+
+        public static void PlayGame(List<string> phrases) {
+            string phrase;
+            phrase = SelectPhrase(phrases);
+            char[] phraseCharacters = GetPhraseCharacters(phrase);
+            HashSet<char> phraseDistinctCharacters = GetPhraseDistinctCharacters(phraseCharacters);
+            List<char> phraseGuessedCharacters = new List<char>();
+            DisplayPhrase(phraseCharacters, phraseGuessedCharacters);
+            Console.Clear();
+            const int maxGuesses = 5;
+            List<char> guessedCharacters = new List<char>();
+
+            while (true) {
+                DisplayPhrase(phraseCharacters, phraseGuessedCharacters);
+                guessedCharacters.Add(GetCharacterGuess(guessedCharacters));
+                if (phraseDistinctCharacters.Contains(guessedCharacters.Last())) {
+                    Console.WriteLine("You chose WISELY. Good Job!");
+                    phraseGuessedCharacters.Add(guessedCharacters.Last());
+                    if (phraseGuessedCharacters.Count == phraseDistinctCharacters.Count) {
+                        Console.WriteLine("You Did It! Congrats.");
+                        Console.WriteLine();
+                        DisplayPhrase(phraseCharacters, phraseGuessedCharacters);
+                        Console.ReadKey();
+                        break;
+                    }
+                    else {
+                        continue;
+                    }
+                }
+                else {
+                    incorrectGuesses += 1;
+                    if (maxGuesses - incorrectGuesses < 0) {
+                        Console.WriteLine("You Lost! Better Luck Next Time.");
+                        Console.WriteLine("The real phrase was: {0}", phrase);
+                        Console.ReadKey();
+                        break;
+                    }
+                    Console.WriteLine("You chose POORLY! You have {0} guesses left", maxGuesses-incorrectGuesses);
+                }
+            }
         }
 
         static void Main(string[] args) {
